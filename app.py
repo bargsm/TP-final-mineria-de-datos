5f204b5681750ca7f98fc01df30bf63843bb445c
mbargs <matias.a.bargas@gmail.com>
2022-06-03 16:31:23 -0300

    preparing deployment

--- app.py
+++ app.py
@@ -5,9 +5,8 @@ from sklearn import preprocessing
 import pickle
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
-cols=['age','workclass','education','marital-status','occupation','relationship','race','gender','capital-gain','capital-loss',
-      'hours-per-week','native-country']
+model = pickle.load(open('model_logistic_2.pkl', 'rb'))
+cols=['age', 'default', 'balance', 'housing', 'duration', 'campaign', 'previous']
 
 @app.route('/')
 def home():
@@ -18,16 +17,16 @@ def predict():
     feature_list = request.form.to_dict()
     feature_list = list(feature_list.values())
     feature_list = list(map(int, feature_list))
-    final_features = np.array(feature_list).reshape(1, 12) 
+    final_features = np.array(feature_list).reshape(1, 7) 
     
     prediction = model.predict(final_features)
     output = int(prediction[0])
     if output == 1:
-        text = ">50K"
+        text = "Resultado 1 : Se convertira en cliente"
     else:
-        text = "<=50K"
+        text = "Resultado 0: No se convertira en cliente"
 
-    return render_template('index.html', prediction_text='Employee Income is {}'.format(text))
+    return render_template('index.html', prediction_text=' {}'.format(text))
 
 
 if __name__ == "__main__":
